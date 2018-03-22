d8b320be8e8e715a1292e69ea8acd12348e5ba7a
Yang Bei <michaelyangzz@msn.cn>
2018-03-22 10:59:28 +0800

    ani

--- src/app/app-ui/app-ui.module.ts
+++ src/app/app-ui/app-ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, BrowserAnimationsModule
   ],
   declarations: [SearchBoxComponent],
   exports: [SearchBoxComponent]
--- src/app/app-ui/search-box/search-box.component.ts
+++ src/app/app-ui/search-box/search-box.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
+  animations: [
+    trigger('heroState', [
+      state('inactive', style({
+        backgroundColor: '#fff',
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        backgroundColor: 'gray',
+        transform: 'scale(1)'
+      })),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class SearchBoxComponent implements OnInit {
 
   constructor() { }
 
+  ss = 'inactive';
+
   ngOnInit() {
   }
 
+  toggle(): void {
+    this.ss = this.ss === 'inactive' ? 'active' : 'inactive';
+  }
 }
